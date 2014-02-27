141c0f31ce29e469ac8bb8e51f6660d1c0626d12
Ningxia Zhang <thmmrs2298@gmail.com>
2014-02-26 19:57:22 -0800

    baseline translate

--- code/translator.py
+++ code/translator.py
@@ -23,6 +23,38 @@ class BaselineTranslator:
       tagged.append(line)
     return tagged
 
+  def tag_tuple(self, sentences):
+    tagged = []
+    for s in sentences:
+      words = pseg.cut(s)
+      line = []
+      for w in words:
+        line.append((w.word, w.flag))
+      tagged.append(line)
+    return tagged
+
+  def translate(self, dictionary, tagged):
+    translated = []
+    for s in tagged:
+      t_sentence = []
+      for word, flag in s:
+        # ignore words unknown to dictionary
+        if word in dictionary:
+          entries = dictionary[word]
+          tag_found = False
+          for e in entries:
+            if e[1] == flag:
+              tag_found = True
+              t_word = e[0]
+
+          # if POS tag not found, use the first translation
+          if not tag_found:
+            t_word = entries[0][0]
+
+          t_sentence.append(t_word)
+      translated.append(t_sentence)
+    return translated
+
 def loadList(file_name):
     """Loads text files as lists of lines. """
     with open(file_name) as f:
@@ -41,13 +73,14 @@ def loadDictionary(file_name):
     word = entries[0]
     dict[word] = []
     for i in xrange(1, len(entries)):
-      entry = entries.split(':')
+      entry = entries[i].split(':')
       dict[word].append([entry[1], entry[0]])
 
   return dict
 
 def main():
   corpus = "../corpus/chinese.txt"
+  dev_file = "../corpus/dev.txt"
   seg_file = "../corpus/segment.txt"
   dictionary_file = "../corpus/dictionary.txt"
 
@@ -61,7 +94,6 @@ def main():
       string_s = " ".join(s)
       f.write(string_s.encode("utf-8"))
       f.write('\n')
-  f.close()
 
   dev = "../corpus/dev.txt"
   output_tagged = "../corpus/dev_tagged.txt"
@@ -72,7 +104,17 @@ def main():
       string_s = " ".join(s)
       f.write(string_s.encode("utf-8"))
       f.write('\n')
-  f.close()
+
+  sentences = loadList(dev_file)
+  tagged_tuples = bl_translator.tag_tuple(sentences)
+  translated = bl_translator.translate(dictionary, tagged_tuples)
+  dev_output = "../output/dev_output.txt"
+  with open(dev_output, "w") as f:
+    for s in translated:
+      string_s = " ".join(s)
+      f.write(string_s.encode("utf-8"))
+      f.write('\n')
+
 
 if __name__ == '__main__':
     main()
